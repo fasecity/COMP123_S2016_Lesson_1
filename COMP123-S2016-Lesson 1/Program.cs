e296ad8a5af932555152a1480b33c6f28f276b0a
audiotrash112@gmail.com <audiotrash112@gmail.com>
2016-05-13 15:12:33 -0400

    commit with notes

--- COMP123-S2016-Lesson 1/Program.cs
+++ COMP123-S2016-Lesson 1/Program.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Author: Mohamoud mohamed
+ * Date Created: May 11, 2016
+ * Date Modified: May 13, 2016
+ * Student ID: 300435435
+ * 
+ * Description: This program shows GitHub and version control
+ * also explore Unit Testing methods
+ * */
 namespace COMP123_S2016_Lesson_1
-{
-   public class Program
-    {
+{/**
+     * This is the driver class for our program
+     * 
+     * @class Program
+     */
+    public class Program
+    { /**
+         * Main Method for class Program
+         * 
+         * @param {string[]} args
+         * @constructor Main
+         */
         static void Main(string[] args)
         {
             HelloToConsole();
         }
-
+        /**
+         * This method outputs a string using a console class
+         * @return write string
+         
+             */
         public static string HelloToConsole()
         {
             string write = "Hello world";
--- UnitTestForComp123/UnitTest_ForCOMP123-Lesson-1.cs
+++ UnitTestForComp123/UnitTest_ForCOMP123-Lesson-1.cs
@@ -3,15 +3,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using COMP123_S2016_Lesson_1;
 
 namespace UnitTestForComp123
-{
+{/**
+     * This class is used to test the Lesson 1 Project
+     * Used for development only.
+     * 
+     * @class Lesson1UnitTest
+     */
     [TestClass]
-    public class UnitTest1
-    {
+    public class UnitTest
+    { /**
+         * Unit Test for HelloToConsole Method of the Program class
+         * 
+         * @method Test1_HelloToConsole
+         */
         [TestMethod]
         public void Test1_HelloToConsole()
         {
+            //arrange
             string expected = "Hello world";
+            //act
             string result = Program.HelloToConsole();
+            //assert
             Assert.AreEqual(expected, result);
         }
     }
